ef6a06ca90d9e38af94b69c4092646a3518a8d3b
HY Tomioka <henriqueyudi2112@gmail.com>
2021-02-03 16:55:43 -0300

    Multiplas insercoes e setAutoCommit=false

--- src/TesteInsercaoComParametro.java
+++ src/TesteInsercaoComParametro.java
@@ -10,29 +10,40 @@ public class TesteInsercaoComParametro {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection conn = connectionFactory.createConnection();
 		
-		String nome = "Mouse";
-		String descricao = "Razer Abyssus";
+		/* AutoCommit em false não realiza o commit das queries automaticamente */
+		conn.setAutoCommit(false);
 		
 		/* O prepareStatement é um método que consegue tratar o comando SQL de forma a evitar
 		 * erros do usuário, assim como evitar SQL injections.
 		 * A consulta neste método é compilada no banco de dados */
-		PreparedStatement stm = conn.prepareStatement(
+		PreparedStatement pstm = conn.prepareStatement(
 				"INSERT INTO PRODUTO(nome, descricao) VALUES(?, ?)",
 				Statement.RETURN_GENERATED_KEYS);
 		
-		stm.setString(1, nome);
-		stm.setString(2, descricao);
+		addVariable("Mouse", "Razer Abyssus", pstm);
+		addVariable("Teclado", "Logitech", pstm);
 		
-		stm.execute();
+		conn.close();
+	}
+
+	private static void addVariable(String nome, String descricao, PreparedStatement pstm)
+			throws SQLException {
+		pstm.setString(1, nome);
+		pstm.setString(2, descricao);
 		
-		ResultSet rs = stm.getGeneratedKeys();
+		/* forçando erro
+		if (nome.equals("Teclado")) {
+			throw new RuntimeException();
+		}
+		*/
+		
+		pstm.execute();
+		
+		ResultSet rs = pstm.getGeneratedKeys();
 		
 		while(rs.next()) {
 			Integer id = rs.getInt(1);
 			System.out.println("id= " + id + " adicionado");
 		}
-		
-		conn.close();
 	}
-
 }
