48428823234d3adeeca9335f8bf0ba14c56f8ffd
HY Tomioka <henriqueyudi2112@gmail.com>
2021-02-03 14:37:04 -0300

    Query de insercao de dados no SQL utilizando java JDBC

---
+++ src/TesteInsercao.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TesteInsercao {
+
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection conn = connectionFactory.createConnection();
+		
+		Statement stm = conn.createStatement();
+		
+		stm.execute("INSERT INTO PRODUTO(NOME, DESCRICAO) VALUES('celular', 'iPhone')",
+				Statement.RETURN_GENERATED_KEYS);
+		
+		ResultSet rs = stm.getGeneratedKeys();
+		while(rs.next()) {
+			Integer id = rs.getInt(1);
+			System.out.println("novo id criado: " + id);
+		}
+		
+		
+
+	}
+
+}
