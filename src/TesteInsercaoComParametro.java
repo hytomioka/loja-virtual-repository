1cb3a2034becc6e6f179c13d7bbe966d34258485
HY Tomioka <henriqueyudi2112@gmail.com>
2021-02-03 18:32:16 -0300

    Implementacao do AutoClosable utilizando try com recurso

--- src/TesteInsercaoComParametro.java
+++ src/TesteInsercaoComParametro.java
@@ -5,59 +5,66 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TesteInsercaoComParametro {
-	
+
 	public static void main(String[] args) throws SQLException {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection conn = connectionFactory.createConnection();
-		
-		/* Aplicação do conceito de transação, no qual, a transação será efetuada somente se,
-		 * tudo o que estava na transação foi realizado com sucesso. Link para consulta:
-		 * https://pt.stackoverflow.com/questions/203857/o-que-%c3%a9-acid-em-banco-de-dados
-		 * AutoCommit em false não realiza o commit das queries automaticamente */
-		conn.setAutoCommit(false);
-		
-		try {
-			/* O prepareStatement é um método que consegue tratar o comando SQL de forma a evitar
-			 * erros do usuário, assim como evitar SQL injections.
-			 * A consulta neste método é compilada no banco de dados */
-			PreparedStatement pstm = conn.prepareStatement(
-					"INSERT INTO PRODUTO(nome, descricao) VALUES(?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-			
-			addVariable("Mouse", "Razer Abyssus", pstm);
-			addVariable("Teclado", "Logitech", pstm);
-			
-			conn.commit();
-			pstm.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			/* ROLLBACK volta o estado original do banco de dados */
-			System.out.println("ROLLBACK executado.");
-			conn.rollback();
+		/*
+		 * O try(recurso) possibilita o fechamento (close()) automático da conexão ou do
+		 * statement, já que estas interfaces herdam o AutoClosable
+		 */
+		try (Connection conn = connectionFactory.createConnection()) {
+
+			/*
+			 * Aplicação do conceito de transação, no qual, a transação será efetuada
+			 * somente se, tudo o que estava na transação foi realizado com sucesso. Link
+			 * para consulta:
+			 * https://pt.stackoverflow.com/questions/203857/o-que-%c3%a9-acid-em-banco-de-
+			 * dados AutoCommit em false não realiza o commit da transação automaticamente
+			 */
+			conn.setAutoCommit(false);
+
+			/*
+			 * O prepareStatement é um método que consegue tratar o comando SQL de forma a
+			 * evitar erros do usuário, assim como evitar SQL injections. A consulta neste
+			 * método é compilada no banco de dados
+			 */
+			try (PreparedStatement pstm = conn.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES(?, ?)",
+					Statement.RETURN_GENERATED_KEYS)) {
+
+				addVariable("Mouse", "Razer Abyssus", pstm);
+				addVariable("Teclado", "Logitech", pstm);
+
+				conn.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				/* ROLLBACK volta o estado original do banco de dados */
+				System.out.println("ROLLBACK executado.");
+				conn.rollback();
+			}
 		}
-		
-		conn.close();
 	}
 
-	private static void addVariable(String nome, String descricao, PreparedStatement pstm)
-			throws SQLException {
+	private static void addVariable(String nome, String descricao, PreparedStatement pstm) throws SQLException {
 		pstm.setString(1, nome);
 		pstm.setString(2, descricao);
-		
+
 //		/* forçando erro
 		if (nome.equals("Teclado")) {
 			throw new RuntimeException("Não foi possivel adicionar o produto.");
 		}
 //		*/
-		
+
 		pstm.execute();
 		
-		ResultSet rs = pstm.getGeneratedKeys();
-		
-		while(rs.next()) {
-			Integer id = rs.getInt(1);
-			System.out.println("id= " + id + " adicionado");
+		/*
+		 * ResultSet é uma interface que também possui o recurso de AutoClosable
+		 */
+		try (ResultSet rs = pstm.getGeneratedKeys()) {
+
+			while (rs.next()) {
+				Integer id = rs.getInt(1);
+				System.out.println("id= " + id + " adicionado");
+			}
 		}
-		
 	}
 }
